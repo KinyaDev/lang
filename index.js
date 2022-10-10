dabd8494b1820a446ca426c946e07e7f696d519f
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2022-10-10 18:43:36 +0200

    ll

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ let av = document.querySelector(".average");
 setInterval(() => {
   av.textContent = getNote();
 }, 3000);
+
+let btnnote = document.querySelector(".sendnote");
+btnnote.addEventListener("click", (ev) => {
+  let inp = document.querySelector(".putnote");
+
+  try {
+    let n = parseInt(inp.value);
+    if (n <= 20) {
+      addNote(inp.value, localStorage.id);
+    } else {
+      alert("Must be under 20");
+    }
+    inp.value = "";
+  } catch {
+    alert("Bad value");
+  }
+});
