78454d70f7757aa0c21a84055b3442bd00c0106a
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2022-10-10 18:48:20 +0200

    Bruh 5

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 let servRoot = "https://nyaara-app.herokuapp.com";
 
-function getNote() {
-  return new Promise(async (resolve, reject) => {
-    let m = await fetch(`${servRoot}/getNotes`);
-    resolve(await m.json());
-  });
-}
-
-function addNote(note, id) {
-  return new Promise(async (resolve, reject) => {
-    await fetch(`${servRoot}/recommend/${id}/${note}`);
-    resolve();
-  });
-}
-
 let av = document.querySelector(".average");
 
-setInterval(() => {
-  av.textContent = getNote();
+setInterval(async () => {
+  let r = await fetch(`${servRoot}/getNotes`);
+  av.textContent = (await r.json()).note;
 }, 3000);
 
 let btnnote = document.querySelector(".sendnote");
@@ -27,7 +14,7 @@ btnnote.addEventListener("click", (ev) => {
   try {
     let n = parseInt(inp.value);
     if (n <= 20) {
-      addNote(inp.value, localStorage.id);
+      fetch(`${servRoot}/recommend/${localStorage.id}/${inp.value}`);
     } else {
       alert("Must be under 20");
     }
